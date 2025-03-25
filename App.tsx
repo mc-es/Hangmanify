5e1f57448130183bbf40c6f5ed75edcd4cca1c47
mc-es <mehmetcan.eser.dev@gmail.com>
2025-03-26 00:23:24 +0300

    init(set-up): add custom ESLint rules to enforce coding standards

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
-const App = () => (
+const App = (): React.JSX.Element => (
   <View style={styles.container}>
     <Text>Open up App.tsx to start working on your app!</Text>
     <StatusBar style="auto" />

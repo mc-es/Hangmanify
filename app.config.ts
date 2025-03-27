ffbb22473ab5e4fa6fb336a1ec6b0a81e2522c53
mces58 <ceser446@gmail.com>
2025-03-27 19:20:34 +0300

    chore(eas-config): move EAS projectId to .env file for security

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => ({
     ...config.extra,
     storybookEnabled: process.env.STORYBOOK_ENABLED as boolean,
     eas: {
-      projectId: 'ce993388-e5cf-41ce-9527-7554d7de672a',
+      projectId: process.env.EAS_PROJECT_ID as string,
     },
   },
 });

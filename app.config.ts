471b8600d572f3a880a2c16b7e5f755f5fbf287b
mces58 <ceser446@gmail.com>
2025-03-27 18:27:12 +0300

    chore(eas-build): add EAS configuration files and workflows for Expo updates

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,10 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => ({
   ...config,
   name: process.env.STORYBOOK_ENABLED ? 'Hangmanify Storybook' : config.name,
   extra: {
+    ...config.extra,
     storybookEnabled: process.env.STORYBOOK_ENABLED as boolean,
+    eas: {
+      projectId: 'ce993388-e5cf-41ce-9527-7554d7de672a',
+    },
   },
 });

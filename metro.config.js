83acff2a766a2f22338ba1477c9e80c41f553579
mc-es <mehmetcan.eser.dev@gmail.com>
2025-03-27 18:27:12 +0300

    chore(eas-build): add EAS configuration files and workflows for Expo updates

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,11 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 const path = require('path');
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = withStorybook(defaultConfig, {
+  ...defaultConfig,
   enabled: process.env.WITH_STORYBOOK,
-  configPath: path.resolve(__dirname, './.storybook'),
+  configPath: path.resolve(__dirname, '.storybook'),
 });

ad6ce49bb8978005e71dc36614501ff3679ee81c
mc-es <mehmetcan.eser.dev@gmail.com>
2025-03-28 15:24:36 +0300

    feat(eslint): add custom rule to validate translation keys

--- plugins/rules/index.mjs
+++ plugins/rules/index.mjs
@@ -2,6 +2,7 @@ import filenameMatchComponent from './filename-match-component.mjs';
 import jsxSortProps from './jsx-sort-props.mjs';
 import requireTryCatchAsync from './require-try-catch-async.mjs';
 import requireUsestateType from './require-usestate-type.mjs';
+import validTranslationKey from './valid-translation-key.mjs';
 
 export default {
   rules: {
@@ -9,5 +10,6 @@ export default {
     'jsx-sort-props': jsxSortProps,
     'require-try-catch-async': requireTryCatchAsync,
     'require-usestate-type': requireUsestateType,
+    'valid-translation-key': validTranslationKey,
   },
 };

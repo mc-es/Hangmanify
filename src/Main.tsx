b917b2dd0e2de34928b57b65180805aeda4ab757
mces58 <ceser446@gmail.com>
2025-03-28 06:08:44 +0300

    feat(localization): configure i18n-js for TR and EN language support

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,15 +3,29 @@ import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Button from './components/buttons/Button';
+import { LANGUAGE_CODES } from './constants/localization';
 import { useTheme } from './contexts/ThemeContext';
+
+import { useI18n } from './contexts';
 
 const Main = (): React.JSX.Element => {
   const { theme, toggleTheme } = useTheme();
+  const { t, toggleI18n, locale } = useI18n();
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: theme.background }]}>
-      <Text style={{ color: theme.text }}>Main</Text>
+      <Text style={{ color: theme.text }}>
+        {t('greetings.hi', { name: 'Anonymous' })}
+      </Text>
+      <Text style={{ color: theme.text }}>{t('greetings.welcome')}</Text>
       <Button onPress={toggleTheme} text="Toggle Theme" />
+      <Button
+        onPress={() => {
+          if (locale === LANGUAGE_CODES.EN) toggleI18n(LANGUAGE_CODES.TR);
+          else toggleI18n(LANGUAGE_CODES.EN);
+        }}
+        text="Toggle I18n"
+      />
     </SafeAreaView>
   );
 };
---
+++ src/contexts/index.ts
@@ -0,0 +1,5 @@
+import { I18nProvider, useI18n } from './I18nContext';
+import { ThemeProvider, useTheme } from './ThemeContext';
+
+export { I18nProvider, useI18n };
+export { ThemeProvider, useTheme };

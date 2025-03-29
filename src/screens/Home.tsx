4160a32d997a9f3b7e64d01e507b41cf26f76c5b
mces58 <ceser446@gmail.com>
2025-03-29 21:24:34 +0300

    feat(zustand): add global state management

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import Button from 'src/components/buttons/Button';
 import type { ScreenNRProps } from 'src/navigations/RootStackParamList';
 import { RootNavigations } from 'src/navigations/RootStackParamList';
+import { useCounter, useGlobalText } from 'src/stores/useStore';
 
 const Home: React.FC<ScreenNRProps['Home']> = ({ navigation }): React.JSX.Element => {
-  const [count, setCount] = useState<number>(0);
+  const { count, increase } = useCounter();
+  const { text, setText } = useGlobalText();
 
   return (
     <View style={styles.container}>
       <Text>Home</Text>
       <Text>{count}</Text>
-      <Button onPress={() => setCount(count + 1)} text="Increment" />
-      <Button
-        onPress={() => navigation.navigate(RootNavigations.ABOUT, { count })}
-        text="About"
+      <Button onPress={increase} text="Increment" />
+      <Button onPress={() => navigation.navigate(RootNavigations.ABOUT)} text="About" />
+      <TextInput
+        onChangeText={(input: string) => setText(input)}
+        placeholder="input"
+        style={{ borderWidth: 1, width: '50%' }}
+        value={text}
       />
     </View>
   );

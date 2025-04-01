380cb973caee9036a1fe65818a2ce1f47f98eab5
mc-es <mehmetcan.eser.dev@gmail.com>
2025-04-01 14:13:22 +0300

    feat(app): add splash screen and app icon

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 import Constants from 'expo-constants';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 
 import Main from 'src/Main';
@@ -11,15 +12,39 @@ import { ThemeProvider } from 'src/contexts/ThemeContext';
 
 import * as Storybook from '.storybook';
 
-const App = (): React.JSX.Element => (
-  <ThemeProvider>
-    <I18nProvider>
-      <View style={{ flex: 1 }}>
-        <StatusBar style="auto" />
-        <Main />
-      </View>
-    </I18nProvider>
-  </ThemeProvider>
-);
+SplashScreen.preventAutoHideAsync().catch((error) => console.error(error));
+
+const App = (): React.JSX.Element => {
+  const [isAppReady, setIsAppReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    const prepare = async (): Promise<void> => {
+      try {
+        // simulated loading time
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsAppReady(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepare();
+  }, []);
+
+  if (!isAppReady) return <></>;
+
+  return (
+    <ThemeProvider>
+      <I18nProvider>
+        <View style={{ flex: 1 }}>
+          <StatusBar style="auto" />
+          <Main />
+        </View>
+      </I18nProvider>
+    </ThemeProvider>
+  );
+};
 
 export default Constants.expoConfig?.extra?.storybookEnabled ? Storybook.default : App;

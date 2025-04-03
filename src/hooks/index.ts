d57c583e809a148a37042be62cf4a079c7475b25
mc-es <mehmetcan.eser.dev@gmail.com>
2025-04-03 21:07:29 +0300

    chore(eslint-rule): add eslint rule to ensure hooks are imported from allowed modules

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @custom-typescript/no-direct-hook-imports */
 import { useI18n } from 'src/contexts/I18nContext';
 import { useTheme } from 'src/contexts/ThemeContext';
 import { useNavigation, useRoute } from 'src/navigations/RootStackParamList';

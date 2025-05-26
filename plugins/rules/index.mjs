0333172572034488ef44fa0f95784ed376447c97
mc-es <mehmetcan.eser.dev@gmail.com>
2025-05-26 23:58:27 +0300

    chore(rule): remove unused custom eslint rule (no-direct-hook-imports)

--- plugins/rules/index.mjs
+++ plugins/rules/index.mjs
@@ -1,7 +1,6 @@
 import filenameMatchComponent from './filename-match-component.mjs';
 import jsxSortProps from './jsx-sort-props.mjs';
 import matchNavigationRoute from './match-navigation-route.mjs';
-import noDirectHookImports from './no-direct-hook-imports.mjs';
 import requireTryCatchAsync from './require-try-catch-async.mjs';
 import requireUsestateType from './require-usestate-type.mjs';
 import validTranslationKey from './valid-translation-key.mjs';
@@ -11,7 +10,6 @@ export default {
     'filename-match-component': filenameMatchComponent,
     'jsx-sort-props': jsxSortProps,
     'match-navigation-route': matchNavigationRoute,
-    'no-direct-hook-imports': noDirectHookImports,
     'require-try-catch-async': requireTryCatchAsync,
     'require-usestate-type': requireUsestateType,
     'valid-translation-key': validTranslationKey,
